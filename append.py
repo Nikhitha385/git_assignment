bec3cb62a10b2ad0ef47ee9d17fbccce88a0d3be
Nikhitha <althinikhitha7789@gmail.com>
2026-08-28 18:45:41 +0530

    Add append Excel program

---
+++ append.py
@@ -0,0 +1,11 @@
+from openpyxl import load_workbook
+
+wb = load_workbook("students.xlsx")
+
+sheet = wb["Students"]
+
+sheet.append(["Ravi", 91, 23])
+
+wb.save("students.xlsx")
+
+print("New student added")
